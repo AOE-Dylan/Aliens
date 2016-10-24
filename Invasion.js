5dbcb3824b21b69f3d67352cd50aeb957039d0d4
AOE-Dylan <dylantt2000@gmail.com>
2016-10-24 08:09:34 -1000

    ALIIEEEEEEEEEEEENNNSSSS

---
+++ Invasion.js
@@ -0,0 +1,15 @@
+var greenAmnt = 9000;
+var redAmnt = 500;
+var blueAmnt = 500;
+
+if (blueAmnt + redAmnt + greenAmnt == 10000){
+  console.log("Its over 9000")
+}
+
+  if (greenAmnt + redAmnt > 8000){
+      console.log("Its Christmas Already?");
+    } else if (greenAmnt > 5000){
+      console.log("Ugh, Vegetables...");
+    } else if (blueAmnt + redAmnt < 3000){
+      console.log("We're all gonna die!!!");
+  }
